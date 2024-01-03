c16c517a4019192d228b5ce57ba2623c372cf673
Nathalie Fajardo <nathaliefc89@gmail.com>
2024-01-03 17:03:48 +0000

    Adding Question Timers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
-  console.log(activeQuestionIndex);
-
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+  
   function handleSelectAnswer(selectecAnswer) {
-    setUserAnswers((prevUserAnswers) => {
-      return [...prevUserAnswers, selectecAnswer];
-    });
-  }
+      setUserAnswers((prevUserAnswers) => {
+          return [...prevUserAnswers, selectecAnswer];
+        });
+    }
+    
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+}
+const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+shuffledAnswers.sort(() => Math.random() - 0.5);
 
-  return (
+return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-            <li key={answer} className="answer">
+          {shuffledAnswers.map((answer) => (
+              <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
               </button>
