a010ece9b918a178ba105e378746233eb3bc7014
Nathalie Fajardo <nathaliefc89@gmail.com>
2024-01-04 16:22:37 +0000

    Setting different timers base on the selected answer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,6 +9,13 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
     isCorrect: null,
   });
 
+  let timer = 10000;
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
   function handleSelectAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
@@ -31,11 +38,16 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
   if (answer.selectedAnswer && answer.isCorrect !== null) {
     answerState = answer.isCorrect ? "correct" : "wrong";
   } else if (answer.selectedAnswer) {
-    answerState = 'answered'
+    answerState = "answered";
   }
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[index].text}</h2>
       <Answers
         answers={QUESTIONS[index].answers}
